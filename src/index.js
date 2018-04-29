37c7cd538b442a0d5e9ba1d7f87e745541b2f7bb
github username <diep.christopher@gmail.com>
2018-04-29 12:03:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,94 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home';
+//Step 1. Import react-router functions
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 
-const Home = () => {
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Home!</h1>
+//     </div>
+//   );
+// };
+
+/* Add basic styling for NavLinks */
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+/* add the navbar component */
+const Navbar = () =>
+  <div>
+    <NavLink
+      to="/"
+      /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+      exact
+      /* add styling to Navlink */
+      style={link}
+      /* add prop for activeStyle */
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Home</NavLink>
+    <NavLink
+      to="/about"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >About</NavLink>
+    <NavLink
+      to="/login"
+      exact
+      style={link}
+      activeStyle={{
+        background: 'darkblue'
+      }}
+    >Login</NavLink>
+  </div>;
+
+
+const About = () => {
   return (
     <div>
-      <h1>Home!</h1>
+      <h1>This is my about component!</h1>
     </div>
-  );
-};
+  )
+}
 
+const Login = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+          <label htmlFor="username">Username</label>
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+          <label htmlFor="password">Password</label>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+// Step 2. Change to have router coordinate with what is displayed: Wrap router around home component & then set the path
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <Navbar />
+      <Route path='/' component={Home} />
+      <Route exact path="/about" render={About} />
+      <Route exact path="/login" render={Login} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
